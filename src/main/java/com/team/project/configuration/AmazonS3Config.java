2ee157d9605d3ea610b6ceadf5f2db3f91acfaaa
g4dalcom <g4dalcom@gmail.com>
2022-11-12 23:22:20 +0900

    AmazonS3config 수정

--- src/main/java/com/team/project/configuration/AmazonS3Config.java
+++ src/main/java/com/team/project/configuration/AmazonS3Config.java
@@ -28,5 +28,6 @@ public class AmazonS3Config {
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .build();
+
     }
 }
